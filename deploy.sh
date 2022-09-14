e7fb1745b3386cf3822d193d735f5cf4148abea9
qalexball <zhuderen12345@icloud.com>
2022-09-14 23:37:44 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 COMMIT_MESSAGE=$1
 GITHUB_IO_PATH=$2
-
+echo $1 $2
 npm run deploy -- -m $COMMIT_MESSAGE
 git add .
 git commit -m $COMMIT_MESSAGE
@@ -8,8 +8,8 @@ git push
 
 # update submodule for github.io
 cd $GITHUB_IO_PATH 
-cd $GITHUB_TO_PATH && git submodule update --remote
-cd $GITHUB_TO_PATH && git add .
-cd $GITHUB_TO_PATH && git commit -m $COMMIT_MESSAGE
-cd $GITHUB_TO_PATH && git push
+git submodule update --remote
+git add .
+git commit -m $COMMIT_MESSAGE
+git push
 
